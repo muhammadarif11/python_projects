d8285bbaf40dcb802e7a498ca736c14ab054ca64
Muhammad Arif <97020897+muhammadarif11@users.noreply.github.com>
2023-01-30 16:57:07 -0500

    Python bill cost and tip calculator

---
+++ Projects/BMI_interpreter.py
@@ -0,0 +1,19 @@
+#defining the variables
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
+
+#BMI calculations
+bmi = round((float(weight/(height*height))))
+
+if bmi < 18.5:
+    print(f"Your BMI is {bmi}, you are underweight.")
+elif bmi < 25 and bmi >18.5:
+    print(f"Your BMI is {bmi}, you have a normal weight.")
+elif bmi > 25 and bmi < 30:
+    print(f"Your BMI is {bmi}, you are slightly overweight.")
+elif bmi > 30 and bmi <35:
+    print(f"Your BMI is {bmi}, you are obese.")
+elif bmi > 35:
+    print(f"Your BMI is {bmi}, you are clinically obese.")
+
+
